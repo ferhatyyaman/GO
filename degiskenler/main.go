7c925c8ae1479b3e2d9863d19c7ec3f45184b9c2
ferhatyyaman <66822481+ferhatyyaman@users.noreply.github.com>
2021-09-10 13:24:14 +0300

    Create main.go

---
+++ degiskenler/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	name := "ferhat"
+	age := 25
+	isMarried := true
+
+	/*  -------uzun yolu-------
+	var name string = "ferhat"
+	var age int = 22
+	var isMarried =true
+
+	*/
+
+	fmt.Println(name)
+	fmt.Println(age)
+	fmt.Println(isMarried)
+}
