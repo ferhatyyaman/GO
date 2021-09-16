b75f150bcfcc79fd6609792974158ded39d1966f
ferhatyyaman <66822481+ferhatyyaman@users.noreply.github.com>
2021-09-16 14:57:34 +0300

    Create main.go

---
+++ ifyapisi_1/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	var hesap float64 = 1000
+	var cekilmekIstenen float64 = 900
+
+	if cekilmekIstenen > hesap {
+		fmt.Println("hesaptaki para yetersiz")
+	}
+	if cekilmekIstenen < hesap {
+		fmt.Println("paranız hazırlanıyor ")
+		hesap = hesap - cekilmekIstenen
+
+	}
+	fmt.Printf("hesapta kalan para: %v", hesap) 
+	//-----------2. yöntem---------
+	//fmt.Println("hesapta kalan para : "+fmt.sprintf(%v,hesap))
+}
+}
